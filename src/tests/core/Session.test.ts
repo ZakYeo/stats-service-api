dfbee0142e64479ea6f18ad4aadbce481498112f
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2025-02-13 12:56:06 +0000

    Added unit test for negative totalModulesStudied

--- src/tests/core/Session.test.ts
+++ src/tests/core/Session.test.ts
@@ -1,4 +1,3 @@
-// tests/core/domain/Session.test.ts
 import { Session } from '../../../src/core/Session';
 
 describe('Session Class', () => {
@@ -15,5 +14,20 @@ describe('Session Class', () => {
         timeStudied: -10
       });
     }).toThrow('timeStudied must be non-negative');
+  });
+
+  it('should throw an error for negative totalModulesStudied', () => {
+    expect(() => {
+      new Session('session-123', -1, 80, 10);
+    }).toThrow('totalModulesStudied must be non-negative');
+
+    expect(() => {
+      new Session.create({
+        sessionID: 'session-123', 
+        totalModulesStudied: -1, 
+        averageScore: 80,
+        timeStudied: 10
+      });
+    }).toThrow('totalModulesStudied must be non-negative');
   });
 });
