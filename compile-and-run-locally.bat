d210291d9984a6af320c7243d2a2d85f1ca91bc3
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2025-02-17 12:12:18 +0000

    Renamed scripts to specify local deployment scripts

--- compile-and-run-locally.bat
+++ compile-and-run-locally.bat
@@ -7,9 +7,9 @@ echo Building TypeScript project...
 call npm run build
 
 echo Building AWS SAM application...
-call sam build
+call sam build --template local-template.yaml
 
 echo Starting AWS SAM local API (Press Ctrl+C to stop)...
-call sam local start-api --docker-network local-dev-network
+call sam local start-api --docker-network local-dev-network --template local-template.yaml
 
 endlocal
