d96cedb7f8b95782b69c0ecabac5a9e3b9439109
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2025-03-05 10:02:50 +0000

    Added loggging & metrics to LambdaHandler

--- src/infrastructure/adapters/LambdaHandler.ts
+++ src/infrastructure/adapters/LambdaHandler.ts
@@ -3,8 +3,12 @@ import { Session } from "../../core/Session";
 import { PersistSessionService } from "../../application/PersistSessionService";
 import { Result } from "../../core/ports/SessionRepository";
 import { Tracer } from '@aws-lambda-powertools/tracer';
+import { Logger } from '@aws-lambda-powertools/logger';
+import { Metrics, MetricUnit } from '@aws-lambda-powertools/metrics';
 
-const tracer = new Tracer({ serviceName: 'LambdaHandlerService' });
+const tracer = new Tracer({ serviceName: 'LambdaHandlerTracerService' });
+const logger = new Logger({ serviceName: 'LambdaHandlerLoggerService' });
+const metrics = new Metrics({ namespace: 'LambdaHandlerMetrics' });
 
 export class LambdaHandler {
   private persistSessionService: PersistSessionService;
@@ -15,6 +19,7 @@ export class LambdaHandler {
 
   @tracer.captureMethod()
   async createSession(event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
+      logger.debug(`Lambda Invoked: `, { event })
       const parsedBody = JSON.parse(event.body || "{}");
       let sessionToSave;
       try{
@@ -27,6 +32,9 @@ export class LambdaHandler {
           userID: event.headers?.Userid || event.headers?.userid || ""
         });
       }catch(error){
+        logger.error(`Error occurred when creating Session object: `, { error })
+        metrics.addMetric('CreateSessionSessionObjectCreationFailure', MetricUnit.Count, 1);
+        metrics.publishStoredMetrics();
         return {
           statusCode: 400,
           body: JSON.stringify({error: error instanceof Error ? error.message : new Error(String(error))})
@@ -40,12 +48,18 @@ export class LambdaHandler {
       subsegment?.close();
 
       if (!result.ok) {
+        logger.error(`Error occurred when saving session: `, { ...result.error })
+        metrics.addMetric('CreateSessionPersistSessionServiceSaveSessionFailure', MetricUnit.Count, 1);
+        metrics.publishStoredMetrics();
         return {
           statusCode: 500,
           body: JSON.stringify({ message: result.error?.message }),
         };
       }
 
+      logger.debug(`Successfully saved session: ${parsedBody?.sessionID}`)
+      metrics.addMetric('CreateSessionSuccess', MetricUnit.Count, 1);
+      metrics.publishStoredMetrics();
       return {
         statusCode: 201,
         body: JSON.stringify({ message: "Successfully saved session" }),
@@ -58,6 +72,9 @@ export class LambdaHandler {
     const courseID = event.headers?.Courseid || event.headers?.courseid || "";
 
     if (!userID || !courseID) {
+      logger.debug(`Error occurred when parsing userID or courseID from headers`)
+      metrics.addMetric('FetchCourseLifetimeStatsInvalidHeadersFailure', MetricUnit.Count, 1);
+      metrics.publishStoredMetrics();
       return {
         statusCode: 400,
         body: JSON.stringify({
@@ -75,6 +92,9 @@ export class LambdaHandler {
     subsegment?.close();
 
     if (!result.ok) {
+      logger.debug(`Error occurred when fetching course lifetimestats: `, { ...result.error })
+      metrics.addMetric('FetchCourseLifetimeStatsPersistSessionServiceFailure', MetricUnit.Count, 1);
+      metrics.publishStoredMetrics();
       return {
         statusCode: 500,
         body: JSON.stringify({
@@ -84,6 +104,10 @@ export class LambdaHandler {
       };
     }
 
+
+    logger.debug(`Successfully fetched lifetime course stats for courseID: ${courseID}. userID: ${userID}`)
+    metrics.addMetric('FetchCourseLifetimeStatsSuccess', MetricUnit.Count, 1);
+    metrics.publishStoredMetrics();
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -103,6 +127,9 @@ export class LambdaHandler {
     const userID = event.headers?.Userid || event.headers?.userid || "";
 
     if (!sessionID || !courseID || !userID) {
+      logger.debug(`Error occurred when parsing userID or courseID or sessionID from headers`)
+      metrics.addMetric('FetchSessionInvalidHeadersFailure', MetricUnit.Count, 1);
+      metrics.publishStoredMetrics();
       return {
         statusCode: 400,
         body: JSON.stringify({
@@ -118,6 +145,9 @@ export class LambdaHandler {
     subsegment?.close();
 
     if (!session.ok) {
+      logger.debug(`Error occurred when fetching Session: `, { ...session.error })
+      metrics.addMetric('FindSessionByIDPersistSessionServiceFailure', MetricUnit.Count, 1);
+      metrics.publishStoredMetrics();
       return {
         statusCode: 500,
         body: JSON.stringify({
@@ -128,6 +158,9 @@ export class LambdaHandler {
     }
 
     if (!session.value) {
+      logger.debug(`Error occurred when fetching Session: Could not find session: ${sessionID}` )
+      metrics.addMetric('FindSessionByIDPersistSessionServiceSessionNotFoundFailure', MetricUnit.Count, 1);
+      metrics.publishStoredMetrics();
       return {
         statusCode: 404,
         body: JSON.stringify({
@@ -137,6 +170,9 @@ export class LambdaHandler {
       };
     }
 
+    logger.debug(`Successfully fetched session for courseID: ${courseID}. userID: ${userID}. SessionID: ${sessionID}`)
+    metrics.addMetric('FetchSessionSuccess', MetricUnit.Count, 1);
+    metrics.publishStoredMetrics();
     return {
       statusCode: 200,
       body: JSON.stringify({
